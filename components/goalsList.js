3569fd299d0c9e88941af79de35774fd34dc87cd
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-05-05 22:27:14 -0300

    Fix edit nome tarefa bug

--- components/goalsList.js
+++ components/goalsList.js
@@ -1,17 +1,26 @@
 import React from 'react';
-import { StyleSheet , View, FlatList } from 'react-native';
+import { StyleSheet , View, FlatList, ScrollView } from 'react-native';
 
 import GoalListDay from './goalListDay'
 
 const GoalList = props => {
     return (
-        <FlatList
-        keyExtractor={(item, index) => item.id.toString()}
-        data={props.calendarData.days}
-        renderItem={({item}) =>
-            <GoalListDay name={item.day} id={item.id} tarefas={item.tarefas} newTarefa={props.newTarefa} removeGoal={props.onRemoveGoal} modifyGoal={props.modifyTarefa} modifyNameTarefa={props.modifyNameTarefa} />
-        }
-        />
+            <ScrollView>
+                <FlatList
+                keyExtractor={(item) => item.id.toString()}
+                data={props.calendarData.days}
+                renderItem={({item}) =>
+                    <GoalListDay name={item.day}
+                    id={item.id}
+                    tarefas={item.tarefas}
+                    newTarefa={props.newTarefa}
+                    removeGoal={props.onRemoveGoal}
+                    modifyGoal={props.modifyTarefa}
+                    modifyNameTarefa={props.modifyNameTarefa} />
+                }
+                /> 
+            </ScrollView>
+        
     )
 }
 
