c4f372c5c584eeb91757bf7b6a5b82c837a4c998
Rafael_Rache <rafaelmarquesrache@gmail.com>
2020-05-22 00:18:58 -0300

    Add load( ) in Home_Screen

--- src/screens/home_screen.js
+++ src/screens/home_screen.js
@@ -1,21 +1,39 @@
 import React, {Component} from 'react';
 
-import { Text, View } from 'react-native';
+import { Text, View, AsyncStorage} from 'react-native';
+
+import SetData from '../async-storage/_storeData'
 
 export default class Home_Screen extends Component {
 
     constructor () {
         super();
         this.state = {
+            string: 'oi'
         };
     }
 
+    componentWillMount() {
+        this.load('@String')
+    }
+
+    load = async (key) => {
+        try {
+            const name = await AsyncStorage.getItem(key)
+            if (name !== null) {
+                this.setState({string: name})
+            }
+        } catch (e) {
+          console.error('Failed to load .')
+        }
+    }
 
     render () {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text>Home_Screen</Text>
-        </View>
-        );
+
+        return (
+            <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+                <Text>{this.state.string}</Text>
+            </View>
+            );
+        }
     }
-}
